8fc41d9e1936d9d64af13a160e05ae4a0409c35d
pebbbles <alondra@Kiaras-MacBook-Pro.local>
2024-05-18 16:15:41 -0400

    added const in js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const startButton = document.getElementById('start');
+const quizButton = document.getElementById('quiz');
+const submitButton = document.getElementById('submit');
+const clearScoresButton = document.getElementById('clear-scores-btn');
+const questionElement = document.getElementById('question');
+const answersElement = document.getElementById('answer');
+const initialsElement = document.getElementById('initials');
+const scoreElement = document.getElementById('score');
+const endElement = document.getElementById('end');
+const highScoresElement = document.getElementById('high-scores');
+const highScoresList = document.getElementById('high-scores-list');
+const timeElement = document.getElementById('time');
+
